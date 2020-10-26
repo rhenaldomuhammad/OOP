356cf7d4eb99d440f1f0a49b02cb41eed962a63e
rhenaldomuhammad <71371301+rhenaldomuhammad@users.noreply.github.com>
2020-10-26 12:22:39 +0700

    Create Console.java

---
+++ Pertemuan 6/KT - OOP/com.teminal/Console.java
@@ -0,0 +1,18 @@
+package com.terminal;
+
+
+//visibilitas
+
+public class Console {
+    public static void log(String message){
+        System.out.println(message);
+    }
+}
+
+
+class Terminal {
+    public static void log(String message){
+        System.out.println(message);
+    }
+}
+
