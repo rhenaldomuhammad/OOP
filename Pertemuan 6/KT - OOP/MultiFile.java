7b9490078cec49d06ddd755342aed3be4bad3927
rhenaldomuhammad <71371301+rhenaldomuhammad@users.noreply.github.com>
2020-10-26 12:21:28 +0700

    Create MultiFile.java

---
+++ Pertemuan 6/KT - OOP/MultiFile.java
@@ -0,0 +1,18 @@
+package com.pboreg;
+
+import com.terminal.Console;
+
+class MultiFile {
+    public static void main(String[] args) {
+        Player player1 = new Player("Viper");
+        Player player2 = new Player("Omen");
+        Player player3 = new Player("Reyna");
+        player1.show();
+        player2.show();
+        player3.show();
+
+        Console.log("hallo");
+        Console.log("holaaaaaaaaa");
+
+    }
+}
